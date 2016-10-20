10fc7b3b9213563e4bad0e4f34c46efbd3cc8cab
tevinstein <tevinsa@yahoo.com>
2016-10-20 13:02:52 +0700

    Tevinstein & Ryan

---
+++ server/server.js
@@ -0,0 +1,14 @@
+let express = require('express')
+let app = express()
+let port = process.env.PORT || 3000
+let api = require('./routes/routes.js')
+let bodyParser = require('body-parser')
+let cors = require('cors')
+app.use(cors())
+app.use(bodyParser())
+
+app.use('/api', api)
+
+app.listen(port, function(){
+  console.log('ya udah jalan di port ', port)
+})
