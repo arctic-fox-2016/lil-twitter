865bbb50d08040173a7da52060e2edc519046caf
Sahbana <sahbanalo@gmail.com>
2016-10-24 09:01:53 +0700

    add tweets

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,21 +1,65 @@
 var express = require('express');
 var router = express.Router();
-var tweet = require('../models/tweets')
+var Tweet = require('../models/tweets')
     /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
         title: 'Express'
     });
 });
-router.get('/tweets', function (req, res, next) {
-    res.render('index', {
-        title: 'Express'
-    });
-});
 router.post('/tweets', function (req, res, next) {
-    res.render('index', {
-        title: 'Express'
-    });
-});
+    let newTweet = new Tweets({
+        username: req.body.username,
+        avatar: req.body.avatar,
+        content: req.body.content,
+        createdAt: Date()
+    })
+    let hashtag = req.body.hashtag.split(" ")
+    for (let i in hashtag) {
+        newTweet.hashtag.push(hashtag[i])
+    }
+    newTweet.save(function (err, result) {
+        if (err) {
+            res.json({
+                message: "error",
+                details: err
+            })
+        } else {
+            res.json(result)
+        }
+    })
+})
+
+router.get('/tweets', function (req, res, next) {
+    Tweets.find({}).sort({
+        "createdAt": -1
+    }).exec(function (err, result) {
+        if (err) {
+            res.json({
+                message: "error",
+                details: err
+            })
+        } else {
+            res.json(result)
+        }
+    })
+})
+
+router.get('/tweets/:q', function (req, res, next) {
+    Tweets.find({
+        hashtag: req.params.q
+    }).sort({
+        "createdAt": -1
+    }).exec(function (err, result) {
+        if (err) {
+            res.json({
+                message: "error",
+                details: err
+            })
+        } else {
+            res.json(result)
+        }
+    })
+})
 
 module.exports = router;
