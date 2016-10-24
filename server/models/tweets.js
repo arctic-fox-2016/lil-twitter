9f044a6eec19b1d7458cc6dce8a59804fa3d77e8
Sahbana <sahbanalo@gmail.com>
2016-10-24 08:44:09 +0700

    tweetlist

--- server/models/tweets.js
+++ server/models/tweets.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose')
 mongoose.connect('mongodb://stackx:stackx@ds057816.mlab.com:57816/stackx')
 var Schema = mongoose.Schema
 
-var tweetsSchema = new mongoose.Schema(({
-            username: String,
-            avatar: String,
-            content: String,
-            createdAt: Date,
-            hashtag: [String]
-        })
+var tweetsSchema = new mongoose.Schema({
+    username: String,
+    avatar: String,
+    content: String,
+    createdAt: Date,
+    hashtag: [String]
+})
 
-        var Tweets = mongoose.model('tweets', tweetsSchema)
-        module.exports = Tweets
+var Tweets = mongoose.model('tweets', tweetsSchema)
+module.exports = Tweets
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var tweet = require('../models/tweets')
+    /* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Express'
+    });
+});
+router.get('/tweets', function (req, res, next) {
+    res.render('index', {
+        title: 'Express'
+    });
+});
+router.post('/tweets', function (req, res, next) {
+    res.render('index', {
+        title: 'Express'
+    });
 });
 
 module.exports = router;
